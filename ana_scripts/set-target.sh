c09b3318f2f49c380dbab64f9f99d6609e16e059
Jandovaz <30621864+zuzanaj@users.noreply.github.com>
2019-06-06 11:17:21 +0200

    set-target for bash shell

---
+++ ana_scripts/set-target.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+export target=complex
+export WDIR=`pwd`
+export refeAB=$WDIR/30_70.2.AB.pdb
+export refeAC=$WDIR/30_70.2.AC.pdb
+export refeAD=$WDIR/30_70.2.AD.pdb
+export refeBC=$WDIR/30_70.2.BC.pdb
+export refeBD=$WDIR/30_70.2.BD.pdb
+export refeCD=$WDIR/30_70.2.CD.pdb
+export izoneAB=$WDIR/30_70.2.izoneAB
+export izoneAC=$WDIR/30_70.2.izoneAC
+export izoneAD=$WDIR/30_70.2.izoneAD
+export izoneBC=$WDIR/30_70.2.izoneBC
+export izoneBD=$WDIR/30_70.2.izoneBD
+export izoneCD=$WDIR/30_70.2.izoneCD
+export lzone=$WDIR/30_70.2.lzone
+export CONTACT=$WDIR/30_70.2.contacts5
+export PROFIT=`which profit | awk '{print $NF}'`
